c1fe16fb3cd66e0a4e6e4fbda790b62c9b31fbfe
Kawis77 <krzysztofsiwak77@gmail.com>
2021-11-17 20:58:48 +0100

    small changes in views and controller

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,13 @@ insert into chords (id, name) values (1, 'A');
 insert into chords (id, name) values (2, 'C');
 insert into chords (id, name) values (3, 'G');
 insert into chords (id, name) values (4, 'E');
+
+insert into songs (id , name ,author , text ,user_id) values (1 , 'TNT' , 'AC/DC' , '...', 1 );
+insert into songs (id , name ,author , text ,user_id) values (2 , 'Forever Young' , 'Alphalive' , '...', 1 );
+insert into songs (id , name ,author , text ,user_id) values (3 , 'Venus' , 'Shocking Blue' , '...', 1 );
+insert into songs (id , name ,author , text,user_id) values (4 , 'Take My Breath' , 'Berlin' , '...' , 1 );
+
+insert into songs_chords(songs_id , chords_id) values (1 , 1);
+insert into songs_chords(songs_id , chords_id) values (2 , 2);
+insert into songs_chords(songs_id , chords_id) values (3 , 3);
+insert into songs_chords(songs_id , chords_id) values (4 , 4);
